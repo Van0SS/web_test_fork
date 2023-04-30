4eec8f2f4a630bba948625b0aa60a6d053024a47
Alexey Eliseev <eliseev.co@gmail.com>
2023-04-29 21:56:12 -0700

    push

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 git add . && git commit -m "$@" && git push
-ssh -t -oHostKeyAlgorithms=+ssh-dss root@198.199.78.103 'git pull && ./deploy.sh'
+ssh -t -oHostKeyAlgorithms=+ssh-dss root@198.199.78.103 'cd web_test && git pull && ./deploy.sh'
